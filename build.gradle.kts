8a7b22b7f1368597a8d2541b557fba2d6fa77659
Arman king <armantechgaming@gmail.com>
2025-10-25 23:12:05 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,19 @@
 plugins {
     id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "1.9.20" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.20" apply false  // Now it exists!
 }
 
 buildscript {
     repositories {
         google()
         mavenCentral()
+        gradlePluginPortal()  // Yeh add kar – plugin resolve ke liye zaroori
         maven { url = uri("https://plugins.gradle.org/m2/") }
-        maven { url = uri("https://maven.google.com") }
-        maven { url = uri("https://jitpack.io") }
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.2.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.20")
     }
 }
 
@@ -22,9 +21,8 @@ allprojects {
     repositories {
         google()
         mavenCentral()
+        gradlePluginPortal()  // Yeh bhi add kar
         maven { url = uri("https://plugins.gradle.org/m2/") }
-        maven { url = uri("https://maven.google.com") }
-        maven { url = uri("https://jitpack.io") }
     }
 }
 
