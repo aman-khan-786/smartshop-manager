3c75b4db4f9db042eefe076d8b7ed376a0182fb7
Arman king <armantechgaming@gmail.com>
2025-10-25 23:01:22 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,19 @@
 plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "1.9.+" apply false // Flexible version
+    id("org.jetbrains.kotlin.plugin.compose") version "1.9.20" apply false // Specific version
 }
 
 buildscript {
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://plugins.gradle.org/m2/") } // Fixed syntax for Kotlin DSL
+        maven { url = uri("https://plugins.gradle.org/m2/") }
+        maven { url = uri("https://maven.google.com") } // Extra repo for Compose
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.2.0") // Latest Gradle plugin
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20") // Match with Kotlin version
+        classpath("com.android.tools.build:gradle:8.2.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
     }
 }
 
@@ -20,7 +21,8 @@ allprojects {
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://plugins.gradle.org/m2/") } // Ensure plugin resolution
+        maven { url = uri("https://plugins.gradle.org/m2/") }
+        maven { url = uri("https://maven.google.com") }
     }
 }
 
